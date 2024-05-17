9de3ece4dd2ac656450c51803d53df0ab4c46809
DaLae37 <dalae37@gmail.com>
2024-05-17 16:25:12 +0900

    Backup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.dalae37.amazingattendance"
         minSdk = 32
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
